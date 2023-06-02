ffd5b55a709ecf2254cb5ff70e989cc7ebf8f230
Igor Jozefowicz <igorjozefowicz@gmail.com>
2023-06-03 00:03:02 +0200

    adjustments to Resources file
    - create Resources.h, delete Resources.cpp
    - create enums: Command & CommandResult

--- gipf/Resources.h
+++ gipf/Resources.h
@@ -4,6 +4,20 @@
 #include "Hex.h"
 
 
+enum Command {
+    NONE,
+    LOAD_GAME_BOARD
+};
+
+
+enum CommandResult {
+    BOARD_STATE_OK,
+    WRONG_BLACK_PAWNS_NUMBER,
+    WRONG_WHITE_PAWNS_NUMBER,
+    WRONG_BOARD_ROW_LENGTH
+};
+
+
 namespace std {
     template <> struct hash<Hex> {
         size_t operator()(const Hex& h) const {
