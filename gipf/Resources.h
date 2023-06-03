0823eec1c0047ac034453086ed58d99c9d390afe
Igor Jozefowicz <igorjozefowicz@gmail.com>
2023-06-03 10:13:40 +0200

    develop Resources.h
    - remove Resources.cpp

--- gipf/Resources.h
+++ gipf/Resources.h
@@ -18,13 +18,28 @@ enum CommandResult {
 };
 
 
+//namespace std {
+//    template <> struct hash<Hex> {
+//        size_t operator()(const Hex& h) const {
+//            hash<int> int_hash;
+//            size_t hq = int_hash(h.q);
+//            size_t hr = int_hash(h.r);
+//            return hq ^ (hr + 0x9e3779b9 + (hq << 6) + (hq >> 2));
+//        }
+//    };
+//}
+
+
+
+
 namespace std {
-    template <> struct hash<Hex> {
-        size_t operator()(const Hex& h) const {
-            hash<int> int_hash;
-            size_t hq = int_hash(h.q);
-            size_t hr = int_hash(h.r);
-            return hq ^ (hr + 0x9e3779b9 + (hq << 6) + (hq >> 2));
+    template<>
+    struct hash<Hex> {
+        std::size_t operator()(const Hex& h) const {
+            std::size_t hq = std::hash<int>{}(h.q);
+            std::size_t hr = std::hash<int>{}(h.r);
+            std::size_t hs = std::hash<int>{}(h.s);
+            return hq ^ (hr << 1) ^ (hs << 2);
         }
     };
 }
